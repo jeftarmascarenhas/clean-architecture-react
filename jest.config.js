f6c1cb2f90ef1e4f1396a91ce78e1a46e26d0fc5
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-06-20 15:53:33 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  root: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/**.{ts,tsx}'],
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
