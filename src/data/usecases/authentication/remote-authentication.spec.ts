5c6313ff00dd362ceccc8e027fa2bf7ea6730b8c
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-07-05 00:31:20 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,9 @@
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { AccountModel } from '@/domain/models/account-model'
-import {
-  mockAccountModel,
-  mockAuthentication
-} from '@/domain/test/mock-account'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { HttpPostClientSpy } from '@/data/test'
+import { RemoteAuthentication } from './remote-authentication'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel, mockAuthentication } from '@/domain/test'
 import { AuthenticationParams } from '@/domain/usercases/authentication'
 import faker from 'faker'
 
