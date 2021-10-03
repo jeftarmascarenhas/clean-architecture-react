1e361df39ce107d53c16bc40fc9f35f0bf81fcec
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-03 16:47:54 -0300

    test: ensure MinLengthValidation returns null if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,15 +1,31 @@
-// import faker from 'faker'
+import faker from 'faker'
 import { InvalidLengthError } from '@/validation/errors/min-length-validation-error'
 import { MinLengthValidation } from './min-length-validation'
 
-const makeSut = (field = 'password', minLength = 0): MinLengthValidation =>
-  new MinLengthValidation(field, minLength)
+interface SutTypes {
+  sut: MinLengthValidation
+  errorMessage: InvalidLengthError
+}
+
+const makeSut = (minLength: number): SutTypes => {
+  const field = faker.database.column()
+  const sut = new MinLengthValidation(field, minLength)
+  const errorMessage = new InvalidLengthError(field, minLength)
+  return {
+    sut,
+    errorMessage
+  }
+}
 
 describe('MinLengthValidation', () => {
   test('should return error if value invalid', () => {
-    const sut = makeSut('passowrd', 5)
+    const { sut, errorMessage } = makeSut(5)
     const error = sut.validate('')
-    const errorMessage = new InvalidLengthError('passowrd', 5)
+    expect(error).toEqual(errorMessage)
+  })
+  test('should return error if value length is less than 5', () => {
+    const { sut, errorMessage } = makeSut(4)
+    const error = sut.validate('123')
     expect(error).toEqual(errorMessage)
   })
 })
