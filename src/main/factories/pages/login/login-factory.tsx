6de664fd099520c00c7d3ed0b46d43e716dd0f61
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-04 12:38:37 -0300

    feat: makeLogin as factory for compose the rules of login

---
+++ src/main/factories/pages/login/login-factory.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
+import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
+import { EmailVadation } from '@/validation/validators/email/email-validation'
+import { MinLengthValidation } from '@/validation/validators/min-length/min-length-validation'
+import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
+
+export const makeLogin: React.FC = () => {
+  const url = 'https://fordevs.herokuapp.com/api/login'
+  const remoteAuthentication = new RemoteAuthentication(
+    url,
+    makeAxiosHttpClient()
+  )
+  const validationComposite = new ValidationComposite([
+    new RequiredFieldValidation('email'),
+    new EmailVadation('email'),
+    new MinLengthValidation('password', 5),
+    new RequiredFieldValidation('password')
+  ])
+  return (
+    <Login
+      authentication={remoteAuthentication}
+      validation={validationComposite}
+    />
+  )
+}
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Styles from './login-styles.scss'
 import { Link, useHistory } from 'react-router-dom'
-import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+import {
+  LoginHeader,
+  Footer,
+  Input,
+  FormStatus
+} from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usercases'
@@ -11,10 +16,7 @@ type Props = {
   authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({
-  validation,
-  authentication
-}: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -34,7 +36,9 @@ const Login: React.FC<Props> = ({
 
   const history = useHistory()
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault()
     try {
       if (state.isLoading || state.emailError || state.passwordError) {
@@ -56,16 +60,27 @@ const Login: React.FC<Props> = ({
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-      <form data-testid="form-login" className={Styles.form} onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <Input type='email' name='email' placeholder='Email' />
-        <Input type='password' name='password' placeholder='Password' />
-        <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} type='submit' className={Styles.submit}>
-          Entrar
-        </button>
-        <Link data-testid="signup" to="/signup" className={Styles.link}>Criar conta</Link>
-        <FormStatus />
-      </form>
+        <form
+          data-testid='form-login'
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
+          <h2>Login</h2>
+          <Input type='email' name='email' placeholder='Email' />
+          <Input type='password' name='password' placeholder='Password' />
+          <button
+            data-testid='submit'
+            disabled={!!state.emailError || !!state.passwordError}
+            type='submit'
+            className={Styles.submit}
+          >
+            Entrar
+          </button>
+          <Link data-testid='signup' to='/signup' className={Styles.link}>
+            Criar conta
+          </Link>
+          <FormStatus />
+        </form>
       </Context.Provider>
       <Footer />
     </div>
