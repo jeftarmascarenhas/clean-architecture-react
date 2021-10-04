9f068bf74db4a68fdd209892b5c5886d7e004e5c
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-04 17:46:22 -0300

    refactor: move code to factories

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,28 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
-import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
-import { EmailValidation } from '@/validation/validators/email/email-validation'
-import { MinLengthValidation } from '@/validation/validators/min-length/min-length-validation'
-import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = 'https://fordevs.herokuapp.com/api/login'
-  const remoteAuthentication = new RemoteAuthentication(
-    url,
-    makeAxiosHttpClient()
-  )
-  const validationComposite = new ValidationComposite([
-    new RequiredFieldValidation('email'),
-    new EmailValidation('email'),
-    new MinLengthValidation('password', 5),
-    new RequiredFieldValidation('password')
-  ])
   return (
     <Login
-      authentication={remoteAuthentication}
-      validation={validationComposite}
+      authentication={makeRemoteAuthentication()}
+      validation={makeLoginValidation()}
     />
   )
 }
